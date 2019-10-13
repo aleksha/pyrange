88f58094150c9c9160fa9a563109932e61157722
Alexey Dzyuba <a.dzyuba@gmail.com>
2019-10-13 11:20:42 +0300

    Add helium to registry

--- registry.py
+++ registry.py
@@ -1,5 +1,6 @@
 # (file_name, normal density, list of aliases)
 
 registry = [
-    ("Hydrogen.txt", 0.0000899, ["H", "H2", "Hydrogen", "Водород", "Wasserstoff", "Hydrogène", "Idrogeno", "Hidrógeno"] )
+    ("Hydrogen.txt", 0.0000899, ["H", "H2", "Hydrogen", "Водород", "Wasserstoff", "Hydrogène", "Idrogeno", "Hidrógeno"] ),
+    ("Helium.txt"  , 0.0001753, ["He", "Helium", "Гелий", "Hélium", "Elio", "Helio"] )
 ]
