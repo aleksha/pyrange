d583344d465e59528853131b11825a4b6f56bbd5
Arsen Nuramatov <nuramatov@hotmail.com>
2020-07-06 20:48:36 +0300

    uncommented carbon, graphite and tin, removed todos

--- registry.py
+++ registry.py
@@ -1,19 +1,17 @@
 # (file_name, normal density, list of aliases)
-# TODO: doublecheck the density values
-# TODO: decide what to do with carbons
-# TODO: Tin: alpha or beta?
+
 registry = [
     ("Hydrogen.txt", 0.0000899, ["H", "H2", "Hydrogen", "Водород", "Wasserstoff", "Hydrogène", "Idrogeno", "Hidrógeno"]),
     ("Helium.txt", 0.0001753, ["He", "Helium", "Гелий", "Hélium", "Elio", "Helio"]),
     ("Beryllium.txt", 1.848, ["Be", "Beryllium", "Бериллий", "Béryllium", "Berilio", "Berillio"]),
     ("Aluminum.txt", 2.70, ["Al", "Aluminum", "Aluminium", "Алюминий", "Aluminio", "Alluminio"]),
     ("Argon.txt", 1.784e-3, ["Ar", "Argon", "Аргон", "Argo", "Argón"]),
-    #("Carbon Amorphous (density20gcm3).txt", 2.0, ["Carbon, Углерод"]),
+    ("Carbon Amorphous (density20gcm3).txt", 2.0, ["C", "Carbon", "Углерод", "Kohlenstoff", "Carbono", "Carbonio"]), #
     ("Copper.txt", 8.96, ["Cu", "Copper", "Медь", "Kupfer", "Cobre", "Rame"]),
     ("Gadolinium.txt", 7.9, ["Gd", "Gadolinium", "Гадолиний", "Gadolinio"]),
     ("Germanium.txt", 5.323, ["Ge", "Germanium", "Германий", "Germanio"]),
     ("Gold.txt", 19.30, ["Au", "Gold", "Золото", "Oro"]),
-    #("Graphite (density 17 gcm3).txt", 1.7, ["Graphite, Графит"]),
+    ("Graphite (density 17 gcm3).txt", 1.7, ["Graphite", "Графит", "Graphit", "Grafit", "Grafito", "Grafite"]), #
     ("Iron.txt", 7.874, ["Fe", "Iron", "Железо", "Eisen", "Ferro", "Hierro", "Fierro"]),
     ("Krypton.txt", 0.003749, ["Kr", "Krypton", "Криптон", "Kripton", "Kripto", "Cripto", "Kriptón", "Criptón"]),
     ("Lead.txt", 11.34, ["Pb", "Lead", "Свинец", "Blei", "Plomo", "Piombo"]),
@@ -24,7 +22,7 @@ registry = [
     ("Platinum.txt", 21.45, ["Pt", "Platinum", "Платина", "Platin", "Platino"]),
     ("Silicon.txt", 2.33, ["Si", "Silicon", "Кремний", "Silicium", "Silizium", "Silicio"]),
     ("Silver.txt", 10.49, ["Ag", "Silver", "Серебро", "Silber", "Argento", "Plata"]),
-    #("Tin.txt", ???, ["Sn", "Tin", "Олово", "Zinn", "Stagno", "Estaño"]),
+    ("Tin.txt", 7.265, ["Sn", "Tin", "Олово", "Zinn", "Stagno", "Estaño"]),
     ("Titanium.txt", 4.506, ["Ti", "Titanium", "Титан", "Titanio"]),
     ("Tungsten.txt", 19.3, ["W", "Tungsten", "Wolfram", "Вольфрам", "Tungsteno", "Wolframio", "Volframio", "Wólfram"]),
     ("Uranium.txt", 19.1, ["U", "Uranium", "Уран", "Uran", "Uranio"]),
